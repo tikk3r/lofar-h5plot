7ae9bb17cfe573743c210c2a903878ac180c066f
Frits Sweijen <fritssweijen@hotmail.com>
2020-03-24 18:29:03 +0100

    Update version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='lofar-h5plot',
-    version='2.3.4',
+    version='2.3.5',
     scripts=['h5plot'],
     author='Frits Sweijen',
     author_email='frits.sweijen@gmail.com',
